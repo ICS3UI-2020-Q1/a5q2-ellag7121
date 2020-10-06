f5787a97d185af7912afe6f37623ae384806d581
ellag7121 <ellag7121@wrdsb.ca>
2020-10-06 13:36:02 +0000

    finished a5q2!

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * asks user for a positive integer, tell the user all the numbers from 1 to that number
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //make a scanner to get user input
+    Scanner input = new Scanner(System.in);
+
+    //initialize variables
+    int number; //the number provided by the user
+
+    //ask user for a number
+    System.out.println("Please enter a positive integer");
+    number = input.nextInt();
+
+    //start a loop to count to that number
+    for(int i = 1; i <= number; i++){
+      //print the current number
+      System.out.print(i);
+      //check if comma is needed
+      if(i != number){ //if it is not the last number
+        //print a comma to the screen
+        System.out.print(", ");
+      }
+    }
   }
 }
